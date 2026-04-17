ab0b1b7432d92c7645f53dae9a5e9106daed1d79
MilanNobel22 <aim4therimgaming@gmail.com>
2026-04-17 15:44:27 +0200

    Add web server to keep bot alive

--- Champion_Ultra/index.js
+++ Champion_Ultra/index.js
@@ -1,12 +1,26 @@
 require("dotenv").config();
 
+const express = require("express");
 const fs = require("fs");
 const BotClient = require("./structures/Client");
 
+// -------------------- WEB SERVER (UptimeRobot) --------------------
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Bot is alive");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Webserver draait");
+});
+
+// -------------------- DISCORD BOT --------------------
 const client = new BotClient(process.env.TOKEN);
 
 client.login(process.env.TOKEN);
 
+// -------------------- HANDLERS --------------------
 ["commands", "events", "addons"].forEach(handler => {
   require(`./handlers/${handler}`).init(client);
 });
