ddab9046da7095a00a7a4d54a3056e11f5daaca5
MilanNobel22 <aim4therimgaming@gmail.com>
2026-04-17 15:43:18 +0200

    Switch to using environment variable for token

--- Champion_Ultra/index.js
+++ Champion_Ultra/index.js
@@ -1,11 +1,11 @@
+require("dotenv").config();
+
 const fs = require("fs");
 const BotClient = require("./structures/Client");
-const yaml = require("yaml");
 
-const config = yaml.parse(fs.readFileSync('./configs/config.yml', 'utf8'));
+const client = new BotClient(process.env.TOKEN);
 
-const client = new BotClient(config.general.token);
-client.login(config.general.token);
+client.login(process.env.TOKEN);
 
 ["commands", "events", "addons"].forEach(handler => {
   require(`./handlers/${handler}`).init(client);
